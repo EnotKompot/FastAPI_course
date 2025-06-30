ce1d1541e59effb3ce8e04768973ed79bc17c203
EnotKompot <enotvkruge@gmail.com>
2025-07-01 01:47:34 +0300

    Add: GET handlers for booking api

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Request
 
+from schemas.bookings import BookingSchema
 from src.schemas.bookings import BookingAddRequestSchema, BookingAddSchema
-from src.api.dependencies import DBDep, get_token, get_current_user_id
+from src.api.dependencies import DBDep, UserIDDep
 
 router = APIRouter(
     prefix="/bookings",
@@ -9,13 +10,33 @@ router = APIRouter(
 )
 
 
-@router.post('')
+@router.get("",
+            summary="Показывает все имеющиеся записи по бронированию"
+            )
+async def get_all_bookings(
+        db: DBDep
+):
+    return await db.bookings.get_all()
+
+
+@router.get("/me",
+            summary="Показывает все записи по бронированию для текущего пользователя"
+            )
+async def get_my_bookings(
+        user_id: UserIDDep,
+        db: DBDep
+):
+    return await db.bookings.get_all_filtered(user_id=user_id)
+
+
+@router.post('',
+             summary="Добавляет бронирование для текущего пользователя"
+             )
 async def add_booking(
         booking: BookingAddRequestSchema,
         db: DBDep,
-        request: Request
+        user_id: UserIDDep
 ):
-    user_id = get_current_user_id(get_token(request))
     room_data = await db.rooms.get_one_or_none(id=booking.room_id)
     price = room_data.price * (booking.date_to - booking.date_from).days
     _booking = BookingAddSchema(
