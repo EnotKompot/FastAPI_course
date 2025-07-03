77adcf0ce5fd5183242801b6f5646b31d6e6ddd6
EnotKompot <enotvkruge@gmail.com>
2025-07-04 02:00:15 +0300

    Upd: For hotels API GET using get_filtered_by_time() with limit and offset

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -13,46 +13,25 @@ class HotelsRepository(BaseRepository):
     model = HotelsORM
     schema = HotelSchema
 
-    async def get_all(
-            self,
-            title,
-            location,
-            limit,
-            offset
-    ):
-        query = select(HotelsORM)
-        if title:
-            query = (
-                query
-                .where(HotelsORM.title.ilike(f"%{title.strip()}%"))
-            )
-        if location:
-            query = (
-                query
-                .where(HotelsORM.location.ilike(f"%{location.strip()}%"))
-            )
-
-        query = (
-            query
-            .limit(limit)
-            .offset(offset)
-        )
-        result = await self.session.execute(query)
-        return [HotelSchema.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
-
 
     async def get_filtered_by_time(
             self,
             date_from: date,
-            date_to: date
+            date_to: date,
+            offset: int,
+            limit: int = 5,
     ):
         awailable_rooms = rooms_ids_for_booking(
             date_from=date_from,
             date_to=date_to
         )
+
         hotels_ids = (
             select(RoomsORM.hotel_id)
             .select_from(RoomsORM)
             .filter(RoomsORM.id.in_(awailable_rooms))
+            .limit(limit)
+            .offset(offset)
         )
+
         return await self.get_all_filtered(self.model.id.in_(hotels_ids))
